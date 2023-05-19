e0c40591fcdc4ad66c3c958b6681b26acdc0acc4
Ludmilaboffo <ludmilabboffo@hotmail.com>
2023-05-18 23:42:21 -0300

    Nueva version: cards, botones interactivos y agregados esteticos

---
+++ PORTFOLIO 2/main.js
@@ -0,0 +1,6 @@
+const typed = new Typed('.typed',{
+    strings:[
+        <i class="fuente">'Programadora'</i>, 
+        <i class="fuente">'Fullstack'</i>, 
+        <i class="fuente">'Ludmila'</i>]
+});
